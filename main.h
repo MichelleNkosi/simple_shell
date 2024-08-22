2aaeee4216c2e69f82494e56d66b678640794d89
MichelleNkosi <Michellen.nkosi@gmail.com>
2024-08-22 10:46:02 -0700

    Simple Shell

--- main.h
+++ main.h
@@ -1,7 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 
 int _atoi(char *str);
 int calculate(int num1, int num2, char operator);
+extern char **environ;
 
 #endif /* MAIN_H */
