4b74060cfd4194576a791c70f02717d85c1e36f9
COLLINS OMWOYO <collinsomwoyo@gmail.com>
2024-09-18 16:40:51 +0300

    "Added line_reader function to read a line from stdin"

---
+++ line-reader.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * line_reader - read a line from stdin
+ *
+ * Return: pointer to line content
+ */
+char *read_line(void)
+{
+char *line = NULL;
+size_t bufsize = 0;
+
+if (getline(&line, &bufsize, stdin) == -1)
+{
+if (feof(stdin))
+{
+free(line);
+/* avoid memory leaks as per project info*/
+exit(EXIT_SUCCESS);
+}
+else
+{
+free(line);
+perror("error while reading lines");
+exit(EXIT_FAILURE);
+}
+}
+return (line);
+}
